49403a85fbcfc3221725b59cbebe5baed123171c
Valentino Uberti <vuberti@vuberti-thinkpadp16vgen1.rmtit.csb>
2026-06-14 11:06:47 +0200

    Better external router configuration

--- external-router/frr/vxlan.sh
+++ external-router/frr/vxlan.sh
@@ -1,11 +1,28 @@
-#Bridge
-sudo ip link add br100 type bridge
-sudo ip link set br100 up
+# ================== EVPN VNI 100 Final Config ==================
 
-#VXLAN 100
-sudo ip link add vxlan100 type vxlan id 100 dstport 4789 local 172.19.0.1 nolearning
-sudo ip link set vxlan100 master br100
-sudo ip link set vxlan100 up
+# Bridge (MAC-VRF)
+ip link add br100 type bridge
+ip link set br100 up
 
-# Gateway IP (highly recommended)
-sudo ip addr add 22.100.0.1/16 dev br100
+# Underlay IP on bridge (required)
+ip addr flush dev enp4s0
+ip addr add 172.19.0.1/24 dev br100
+
+
+# VXLAN device (traditional model)
+ip link add vni100 type vxlan id 100 dstport 4789 local 172.19.0.1 nolearning
+ip link set vni100 master br100
+ip link set vni100 up
+
+# Attach physical NIC
+ip link set enp4s0 master br100
+bridge vlan add dev enp4s0 vid 1 pvid untagged
+ip link set enp4s0 up
+
+# Gateway (SVI) - on the bridge (best for traditional model)
+ip addr add 22.100.0.1/24 dev br100
+
+# Optional but recommended: ARP/ND suppression
+ip link set vni100 type bridge_slave neigh_suppress on
+
+
